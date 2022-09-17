cff6e647881622ca4cb3546771624297c6110281
ryutizm <ryutizm1103@gmail.com>
2022-09-18 02:43:33 +0900

    ⚗️ 不要な記述(クリックイベント書いてみた)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,16 +4,34 @@ import classes from 'src/styles/Home.module.css'
 import { Footer } from "src/components/Footer"
 import { Header } from 'src/components/Header'
 import { Main } from "src/components/Main"
+import { useCallback } from 'react'
 
 export default function Home() {
+  // function handleClick (e) {
+const foo = 1;
+
+const handleClick = useCallback((e) => {
+  console.log(e.target.href);
+  e.preventDefault();
+  alert(foo);
+}, []);
+
+  
   return (
     <div className={classes.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a 
+        href="/about"
+        onClick={handleClick}
+      >
+        ボタン
+      </a>
+
       <Main page="index" />
       <Footer />
     </div>
-  )
-}
+  );
+};
