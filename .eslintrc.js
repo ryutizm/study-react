977250bc0f8697364103e3318c8a5e4abd0669cc
ryutizm <ryutizm1103@gmail.com>
2022-09-21 06:04:16 +0900

    ⏪ eslintの設定・prettierの設定

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    node: "true",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parserOptions: {
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "no-var": "error",
+  },
+};
