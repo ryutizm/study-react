19db150efc81a6025fb6a19db29f2eac8f3794ec
ryutizm <ryutizm1103@gmail.com>
2022-09-29 00:43:05 +0900

    ✨ useStateで文字列と真偽値について学んだ

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,8 @@ import { useCallback, useEffect, useState } from "react";
 export default function Home() {
   // 前の状態を用いた処理をしたい場合の書き方
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -17,6 +19,18 @@ export default function Home() {
       // ↑これだと直接代入になっていて、前の状態を引き継いでいない
     }
   }, [count]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください。");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -31,8 +45,10 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
