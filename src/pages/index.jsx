3e7b1d10cd5680155b011d079f11b9036f4e3183
ryutizm <ryutizm1103@gmail.com>
2022-09-30 02:02:14 +0900

    ♻️ 状態を変更するときにprevを付けて名前をわかりやすくした

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,14 +14,14 @@ export default function Home() {
 
   const handleClick = useCallback(() => {
     if (count < 10) {
-      setCount((count) => count + 1);
+      setCount((prevCount) => prevCount + 1);
       // setFoo(foo + 1);
       // ↑これだと直接代入になっていて、前の状態を引き継いでいない
     }
   }, [count]);
 
   const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
+    setIsShow((prevIsShow) => !prevIsShow);
   }, []);
 
   const handleChange = useCallback((e) => {
