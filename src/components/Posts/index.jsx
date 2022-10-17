de0fe4080e405e781b1ba9da9f97a0edc41fc794
ryutizm <ryutizm1103@gmail.com>
2022-10-17 22:41:01 +0900

    🚸 APIを叩く際のローディング状態・エラー状態を追加

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,48 @@
+import { useCallback, useEffect, useState } from "react";
+import { Home } from "src/pages/index";
+
+export const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getPosts = useCallback(async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      if (!res.ok) {
+        throw new Error("エラーが発生したためデータの取得に失敗しました。");
+      }
+      const json = await res.json();
+      setPosts(json);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  if (loading) {
+    return <div>ローディング中</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (posts.length === 0) {
+    return <div>データがありません。</div>;
+  }
+
+  return (
+    <ol>
+      {posts.map((post) => {
+        return <li key={post.id}>{post.title}</li>;
+      })}
+    </ol>
+  );
+};
+
+export default Home;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,44 +1,18 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/react-in-jsx-scope */
 import Head from "next/head";
 import classes from "src/styles/Home.module.css";
-import { Footer } from "src/components/Footer";
+import { Posts } from "src/components/Posts";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
-import { useCallback, useEffect, useState } from "react";
 
 // eslint-disable-next-line no-unused-vars
-const Home = (props) => {
-  const [posts, setPosts] = useState([]);
-
-  const getPosts = useCallback(async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const json = await res.json();
-    setPosts(json);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
-  console.log(posts);
-
+const Home = () => {
   return (
     <div className={classes.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <ol>
-        {posts.map((post) => {
-          return <li key={post.id}>{post.title}</li>;
-        })}
-      </ol>
-
-      <Main />
-      <Footer />
+      <Posts />
     </div>
-
   );
 };
 
